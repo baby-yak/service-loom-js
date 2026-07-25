fe8adcabac0eb72fc0a95d90836d78934a0887cd
yakush <its.yak@gmail.com>
2026-07-25 17:32:13 +0300

    condensed unnessesary types

--- packages/service-loom-js/src/actions/actionClient.ts
+++ packages/service-loom-js/src/actions/actionClient.ts
@@ -1,7 +1,10 @@
 import type { _BRAND_ACTION_EXECUTER_CLIENT } from '../core/internal/symbols.js';
-import type { ActionMap, Invoker } from './types.js';
+import type { ActionMap } from './types.js';
 
+// mapped function + branding fields
 export type ActionClient<T_Map extends ActionMap> = //
-  Invoker<T_Map> & { // actions
+  {
+    [K in keyof T_Map]: T_Map[K];
+  } & {
     [_BRAND_ACTION_EXECUTER_CLIENT]: true; // branding
   };
